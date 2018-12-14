546cb64a5044df4c813d57156863cc8b2536a0ba
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2018-12-14 18:19:16 +0200

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,42 +1,50 @@
 #include "Graph.h"
 #include<iostream>
 using namespace std;
-Edge::Edge()
-{
-	 weight=0;
-	 src="";
-	 dest="";
-}
+
 Graph::Graph(int num)
 {
-	EdgesNum = num;
+	numEdges = num;
 }
 void Graph::addGraph(int num)
 {
 	for (int i = 0; i < num; ++i)
-		addEdge();
+	{
+		cin >> edge.src >> edge.dest >> edge.weight;
+		addEdge(edge.src, edge.dest, edge.weight);
+	}		
 }
 void Graph::addVertex(string ver)
 {
-	
 	if (adjlist.find(ver) == adjlist.end())
 		adjlist[ver];
 }
-void Graph::addEdge()
-{   cin >>edge.src;
-	cin >>edge.dest;
-	cin >>edge.weight;
-	adjlist[edge.src].push_back(make_pair(edge.dest,edge. weight));
-	if (adjlist.find(edge.dest) == adjlist.end())
-		adjlist[edge.dest];
+void Graph::addEdge(string src, string dest, int weight)
+{
+	adjlist[src].push_back(make_pair(dest, weight));
+	if (adjlist.find(dest) == adjlist.end())
+		adjlist[dest];
+}
+bool Graph::areAdjacent(string src, string dest)
+{
+	unordered_map <string, list<pair<string, int>>>::iterator it1 = adjlist.find(src);
+	list<pair<string, int>>::iterator it2;
+	for (list<pair<string, int>>::iterator it2 = it1->second.begin(); it2 != it1->second.end(); it2++)
+	if (it2->first == dest)
+		return true;
+	return false;
+}
+int Graph::numVertices()
+{
+	return(adjlist.size());
 }
 void Graph::displayGraph(int num)
 {
-	for (auto it = adjlist.begin(); it != adjlist.end(); ++it){
+	for (auto it = adjlist.begin(); it != adjlist.end(); ++it)
+	{
 		cout << it->first << " ";
-		for (list<pair<string, int>>::iterator s = it->second.begin(); s != it->second.end(); ++s){
+		for (list<pair<string, int>>::iterator s = it->second.begin(); s != it->second.end(); ++s)
 			cout << s->first << " " << s->second << " ";
-		}
 		cout << endl;
 	}
 }
@@ -47,8 +55,4 @@ void Graph::deleteGraph()
 Graph::~Graph()
 {
 	deleteGraph();
-	
-}
-Edge::~Edge()
-{
 }
