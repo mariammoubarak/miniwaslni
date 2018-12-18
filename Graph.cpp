dd203242901a8c47ae1e56b59b8e810d3b0b60be
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2018-12-18 16:54:34 +0200

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,6 +1,11 @@
 #include "Graph.h"
 #include<iostream>
+#include <queue>
+#include <vector>
+#include <functional> //for greater, for sorting pq ascending
+#define INF  INT_MAX
 using namespace std;
+typedef pair <int, string> dijkstraPair; //first of pair is the minimum distance, to ensure that pq is sorted acc. to distance. While dest. name is second of pair.
 
 Graph::Graph()
 {
@@ -13,7 +18,7 @@ void Graph::addVertex(string ver)
 }
 void Graph::addEdge(string src, string dest, int weight)
 {
-	//if edge already exists,update weight 
+	//if edge already exists, update weight 
 	if (numEdges > 0 && areAdjacent(src, dest))
 	{
 		updateWeight(src, dest, weight);
@@ -38,75 +43,58 @@ void Graph::deleteVertex(string vertex)
 			{
 				//adjlist[it->first].remove(*o);
 				adjList[it->first].erase(o);
-				int x = it->second.size();
 				break; //to avoid dereferincing an iterator to a non existing list
 			}
 		}
-			 //deleting vertex as source
+		//deleting vertex as source
 	    if (it->first == vertex)
-		    {
-				adjList.find(vertex)->second.clear();
-				adjList.erase(vertex);
-			}
+	    {
+			adjList.find(vertex)->second.clear();
+			adjList.erase(vertex);
+		}
 	}
 }
 void Graph::deleteEdge(string src, string dest)
 {
 	if (adjList.find(src) == adjList.end())
 		return;
-	for (auto del = adjList.find(src)->second.begin(); del != adjList.find(src)->second.end(); ++del)
+	for (auto del = adjList[src].begin(); del != adjList[src].end(); ++del)
 		if (del->first == dest)
 		{
 			adjList[src].remove(*del);
-			numEdges--;
-			//adjlist[source].erase(del);
+			//adjList[source].erase(del);
+			--numEdges;
 			return;
 		}
 }
 void Graph::updateWeight(string src, string dest, int weight)
 {
-	bool found = false;
-	for (auto move = adjList.find(src)->second.begin(); move != adjList.find(src)->second.end(); ++move)
+	if (adjList.find(src) == adjList.end())
+		return;
+	for (auto move = adjList[src].begin(); move != adjList[src].end(); ++move)
 	{
 		if (move->first == dest)
 		{
 			move->second = weight;
-			found = true;
 			return;
 		}
 	}
 	//if edge not found, offer to add it
-	if (!found)
-	{
-		char ch;
-		cout << " add it (Y|N) ? \n";
-		cin >> ch;
-		if (ch == 'y')
-			addEdge(src, dest, weight);
-		else
-			return;
-	}
+	char ch;
+	cout << " add it (Y|N) ? \n";
+	cin >> ch;
+	if (ch == 'y')
+		addEdge(src, dest, weight);
 }
-//void Graph::Update()
-//{
-//	int ch;
-//	cout << " 1 -> edit path , 2 -> add source , 3 -> add dest, 4 -> delete source , 3 -> delete dest ";
-//	cin >> ch;
-//	cin >> edge.src >> edge.dest >> edge.weight;
-//	if (ch == 1)
-//		updateWeight(edge.src, edge.dest, edge.weight);
-//	else if (ch == 4)
-//		deleteVertex(edge.src);
-//	else if (ch == 5)
-//		deleteEdge(edge.src, edge.dest);
-//}
 //check if two nodes are neighbours
 bool Graph::areAdjacent(string src, string dest)
 {    
-		for (auto it2 = adjList.find(src)->second.begin(); it2 != adjList.find(src)->second.end(); it2++)
+	if (adjList.find(src) == adjList.end())
+		return false;
+	for (auto it2 = adjList[src].begin(); it2 != adjList[src].end(); it2++)
 		if (it2->first == dest)
 			return true;
-		return false;	
+	return false;	
 }
 int Graph::numVertices()
 {
@@ -118,7 +106,7 @@ void Graph::displayGraph()
 	{
 		for (auto it = adjList.begin(); it != adjList.end(); ++it)
 		{
-			if (it->second.empty())
+			if (it->second.empty()) //vertex is only a destination
 				continue;
 			cout << it->first << " ";
 			for (auto s = it->second.begin(); s != it->second.end(); ++s)
