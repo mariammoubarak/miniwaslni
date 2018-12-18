995637c8711e64f91894546cf1aae5d0ec2c9c22
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2018-12-18 16:36:17 +0200

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -3,25 +3,20 @@
 #include<list>
 #include<unordered_map>
 using namespace std;
-class Edge
-{
-public:
-	int weight;
-	string src, dest;
-};
+
 class Graph
 {
 	int numEdges;
 	unordered_map <string, list<pair<string, int>>> adjList;
-	Edge edge;
+
 public:
 	Graph();
 	void addEdge(string, string, int);
 	void addVertex(string);
-	//void Update();
 	void deleteVertex(string);
 	void deleteEdge(string, string);
 	void updateWeight(string, string, int);
+	void dijkstra(string, string);
 	bool areAdjacent(string, string);
 	int numVertices();
 	void displayGraph();
