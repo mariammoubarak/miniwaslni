5f3c5c9b88b683f58ac93cf316d8d056a11444e3
NayeraMohamed <32566231+NayeraMohamed@users.noreply.github.com>
2018-12-22 18:09:44 +0200

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -22,7 +22,7 @@ public:
 	bool exists(string, string);
 	int dijkstra(string, string, stack<string>&);
 	int Bellman(string, string, stack<string>&);
-	stack<string> buildPath(unordered_map<string, string>, string);
+	stack<string> buildPath(unordered_map<string, pair<int, string>>, string, string);
 	void getIndex();
 	void DFS(string);
 	void displayGraph();
